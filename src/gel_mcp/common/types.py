0bc5be3461db0db1147d6de43ccb0f8f632f2507
deepbuzin <contactbuzin@gmail.com>
2025-03-26 20:11:45 +0700

    Fix bug in code examples formatting

--- src/gel_mcp/common/types.py
+++ src/gel_mcp/common/types.py
@@ -11,7 +11,7 @@ a proper database.
 class CodeSnippet(BaseModel):
     """A piece of code. Can be an entire file or a snippet from a file."""
 
-    id: str 
+    id: str
     url: str | None = None
     code: str | None = None
     language: str | None = None
@@ -20,7 +20,7 @@ class CodeSnippet(BaseModel):
 class Example(BaseModel):
     """An example served to the agent via the MCP server to help it complete the workflow."""
 
-    id: str 
+    id: str
     name: str | None = None
     slug: str | None = None
     description: str | None = None
@@ -31,7 +31,7 @@ class Example(BaseModel):
 class Test(BaseModel):
     """A test case to manually run against the agent"""
 
-    id: str 
+    id: str
     test_prompt: str | None = None
     expected_outcome: str | None = None
     initial_state: list[CodeSnippet] = Field(default_factory=list)
@@ -40,11 +40,10 @@ class Test(BaseModel):
 class Workflow(BaseModel):
     """A workflow that describes implementing an app feature using Gel"""
 
-    id: str 
+    id: str
     name: str | None = None
     tests: list[Test] = Field(default_factory=list)
     examples: list[Example] = Field(default_factory=list)
-
 
 
 """
@@ -71,7 +70,6 @@ class MCPExample(Example):
 
     @classmethod
     def from_workflow_example(cls, example: Example) -> "MCPExample":
-
         def name_to_slug(name: str) -> str:
             short_name = name.lower().split(" ")[:5]
             return "-".join(short_name)
@@ -91,14 +89,17 @@ class MCPExample(Example):
         formatted_code = []
         for code_snippet in self.code:
             formatted_code.append(
-                f"From {code_snippet.url}\n" if code_snippet.url else ""
+                f"From {code_snippet.url}\n"
+                if code_snippet.url
+                else ""
                 + MARKDOWN_CODE_SNIPPET_TEMPLATE.format(
                     language=code_snippet.language,
                     code=code_snippet.code,
                 )
             )
-        
-        body = self.instructions + "\n\n" + "\n\n".join(formatted_code)
+
+        body = self.instructions if self.instructions else ""
+        body += "\n".join(formatted_code)
 
         return MARKDOWN_EXAMPLE_TEMPLATE.format(
             name=self.name,
